cfbc183eee1c3951afe71e18a6b9eee13b0ccd5e
EJ Layne <ej.layne@playonsports.com>
2013-08-29 23:19:50 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Component::Application.config.secret_key_base = '4f6c67e91ccbc4812c6f054165af3cbe9c8f7db0d869db127ac23f9650f8dfd04b0b80661407148c989a450ae34c01941fc2e91177330bf0ec66dbd4b2fe9840'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Component::Application.config.session_store :cookie_store, key: '_component_session'
