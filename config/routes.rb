934c440115ab8106771a4de8685cbcb2544523a0
EJ Layne <ej.layne@playonsports.com>
2013-09-02 23:25:09 -0700

    added user model, controller, routes, and migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Component::Application.routes.draw do
-  get "home/index"
+  resources :users
+
+  root :to => "home#index"
+  get '/', to: 'home#index', as: :home
+
+  get 'users/index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
